03bb2783edaf2264fba8c4f9b53ab9e032bf4895
Gerui <ligerui@outlook.com>
2022-08-08 02:28:24 -0700

    Updates

---
+++ precache-manifest.3e37d0226b128ca81ec3d9d5d4b36324.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c09924cdfcab821b67381a39ec89657",
+    "url": "/index.html"
+  },
+  {
+    "revision": "309dc36d82748fb22f17",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "309dc36d82748fb22f17",
+    "url": "/static/js/main.b120729e.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "49bafd210479c8bc6e2ddb0e9ee787ae",
+    "url": "/static/media/GeruiLi.49bafd21.png"
+  },
+  {
+    "revision": "84a559c4e15bb165d85cc2da499dcc85",
+    "url": "/static/media/resume.84a559c4.pdf"
+  }
+]);
